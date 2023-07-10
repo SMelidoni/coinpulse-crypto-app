25754c9a5370f3596fb80de606b0fa53d1b07cc7
SMelidoni <simone.melidoni@gmail.com>
2023-07-10 17:54:58 +0100

    Updated home title styles

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,12 +1,11 @@
 import './home.styles.scss';
-import React, { FC, useState } from 'react';
-import { useEffect, useRef } from 'react';
+import React, { FC } from 'react';
 
 const Home: FC = () => {
 	return (
 		<section id='home' className='home-section'>
 			<div className='home-container'>
-				<h1>Track Cryptocurrencies</h1>
+				<h1 className='title'>Track Crypto-currencies</h1>
 			</div>
 		</section>
 	);
