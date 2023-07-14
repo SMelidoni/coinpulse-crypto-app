9ed09fb45886204bbccbdb79b73d856a83457dce
SMelidoni <simone.melidoni@gmail.com>
2023-07-14 18:22:33 +0100

    Install axios and initial file setup

---
+++ src/components/fear-greed-index/fear-greed-index.component.tsx
@@ -0,0 +1,8 @@
+import './fear-greed-index.styles.scss';
+import React, { FC } from 'react';
+
+const FearGreedIndex: FC = () => {
+	return <div>Fear and Greed Index</div>;
+};
+
+export default FearGreedIndex;
