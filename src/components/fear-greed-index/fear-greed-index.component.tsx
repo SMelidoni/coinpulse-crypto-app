ba605f43164cbeb13c42d69fc6fb5ac0aae53942
SMelidoni <simone.melidoni@gmail.com>
2023-07-15 13:47:11 +0100

    Added time until next update for F.A.G index

--- src/components/fear-greed-index/fear-greed-index.component.tsx
+++ src/components/fear-greed-index/fear-greed-index.component.tsx
@@ -1,32 +1,60 @@
 import './fear-greed-index.styles.scss';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 
 type FearGreedData = {
 	value: string;
 	value_classification: string;
+	time_until_update: string;
 };
 
 const FearGreedIndex = () => {
 	const [data, setData] = useState<FearGreedData | null>(null);
+	const [countDown, setCountDown] = useState<number | null>(null);
 
 	const url = `https://api.alternative.me/fng/?limit=0`;
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await axios.get(url);
-				console.log(data);
-				const fearGreedData: FearGreedData = response.data.data[0];
-				setData(fearGreedData);
-			} catch (error) {
-				console.error(error);
-			}
-		};
+	const timeInHMS = (seconds: number | null) => {
+		if (!seconds) return;
+		const hours = Math.floor(seconds / 3600);
+		const minutes = Math.floor((seconds - hours * 3600) / 60);
+		const remainingSeconds = seconds - hours * 3600 - minutes * 60;
+		return `${hours}h ${minutes}m ${remainingSeconds}s`;
+	};
 
+	const fetchData = useCallback(async () => {
+		try {
+			const response = await axios.get(url);
+			const fearGreedData: FearGreedData = response.data.data[0];
+			setData(fearGreedData);
+			if (fearGreedData.time_until_update) {
+				setCountDown(parseInt(fearGreedData.time_until_update));
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}, [url]);
+
+	useEffect(() => {
 		fetchData();
-	}, [data, url]);
+	}, [fetchData]);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCountDown((prev) => (prev && prev > 0 ? prev - 1 : 0));
+		}, 1000);
+
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (countDown === 0) {
+			fetchData();
+		}
+	}, [countDown, fetchData]);
 
 	return (
 		<div
@@ -40,6 +68,7 @@ const FearGreedIndex = () => {
 			<div className='fear-greed-name'>
 				<p>{data?.value_classification}</p>
 			</div>
+			{countDown !== null && <div>Next update in: {timeInHMS(countDown)}</div>}
 		</div>
 	);
 };
