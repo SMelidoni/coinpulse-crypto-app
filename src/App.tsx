38adcc0f21e0050b424b9e7bd09fa1c332b9c045
Simone Melidoni <simone.melidoni@gmail.com>
2023-07-16 08:57:43 +0100

    feature/fear-and-greed-index (#5)
    * Install axios and initial file setup
    
    * Added initial fear and greed index
    
    * Updated TODO.md
    
    * Added time until next update for F.A.G index
    
    * Updated CHANGELOG.md

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.scss';
 
 import React from 'react';
 import Home from './pages/home/home.component';
+import FearGreedIndex from './components/fear-greed-index/fear-greed-index.component';
 
 function App() {
 	return (
 		<div className='App'>
 			<Home />
+			<FearGreedIndex />
 		</div>
 	);
 }
---
+++ src/components/fear-greed-index/fear-greed-index.component.tsx
@@ -0,0 +1,76 @@
+import './fear-greed-index.styles.scss';
+
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
+
+type FearGreedData = {
+	value: string;
+	value_classification: string;
+	time_until_update: string;
+};
+
+const FearGreedIndex = () => {
+	const [data, setData] = useState<FearGreedData | null>(null);
+	const [countDown, setCountDown] = useState<number | null>(null);
+
+	const url = `https://api.alternative.me/fng/?limit=0`;
+
+	const timeInHMS = (seconds: number | null) => {
+		if (!seconds) return;
+		const hours = Math.floor(seconds / 3600);
+		const minutes = Math.floor((seconds - hours * 3600) / 60);
+		const remainingSeconds = seconds - hours * 3600 - minutes * 60;
+		return `${hours}h ${minutes}m ${remainingSeconds}s`;
+	};
+
+	const fetchData = useCallback(async () => {
+		try {
+			const response = await axios.get(url);
+			const fearGreedData: FearGreedData = response.data.data[0];
+			setData(fearGreedData);
+			if (fearGreedData.time_until_update) {
+				setCountDown(parseInt(fearGreedData.time_until_update));
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}, [url]);
+
+	useEffect(() => {
+		fetchData();
+	}, [fetchData]);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCountDown((prev) => (prev && prev > 0 ? prev - 1 : 0));
+		}, 1000);
+
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (countDown === 0) {
+			fetchData();
+		}
+	}, [countDown, fetchData]);
+
+	return (
+		<div
+			className={`fear-greed-index ${data?.value_classification.replace(
+				' ',
+				'-',
+			)}`}
+		>
+			<h2>Fear & Greed Index</h2>
+			<div className='fear-greed-value'>{data?.value}</div>
+			<div className='fear-greed-name'>
+				<p>{data?.value_classification}</p>
+			</div>
+			{countDown !== null && <div>Next update in: {timeInHMS(countDown)}</div>}
+		</div>
+	);
+};
+
+export default FearGreedIndex;
