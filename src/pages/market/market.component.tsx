d1435892a983de8044b68c14e3d7f333077d2117
SMelidoni <simone.melidoni@gmail.com>
2023-07-30 12:46:45 +0100

    Initial section page setup

---
+++ src/pages/market/market.component.tsx
@@ -0,0 +1,14 @@
+import './market.styles.scss';
+import React, { FC } from 'react';
+
+const Market: FC = () => {
+	return (
+		<section id='market' className='market-section'>
+			<div className='market-container'>
+				<h1 className='title'>Market Update</h1>
+			</div>
+		</section>
+	);
+};
+
+export default Market;
