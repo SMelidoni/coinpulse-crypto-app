a4ed5d1d9fc34a3471a92f6c87b134b8b8b7b367
SMelidoni <simone.melidoni@gmail.com>
2023-08-09 19:38:48 +0100

    Added ability to sort market table columns

--- src/pages/market/market.component.tsx
+++ src/pages/market/market.component.tsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 const Market: FC = () => {
 	const [coinData, setCoinData] = useState<ICoinData[]>([]);
 	const [currentPage, setCurrentPage] = useState(1);
+	const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+	const [sortField, setSortField] = useState<keyof ICoinData | null>(null);
 
 	const coinsPerPage = 10;
 	const indexOfLastCoin = currentPage * coinsPerPage;
@@ -48,6 +50,29 @@ const Market: FC = () => {
 		setCurrentPage(pageNumber);
 	};
 
+	const handleSort = (field: keyof ICoinData | null) => {
+		if (!field) return;
+
+		const newSortOrder =
+			sortField === field ? (sortOrder === 'asc' ? 'desc' : 'asc') : 'asc';
+
+		const sortedData = [...coinData].sort((a, b) => {
+			if (field === 'name') {
+				return newSortOrder === 'asc'
+					? a[field].localeCompare(b[field])
+					: b[field].localeCompare(a[field]);
+			} else {
+				return newSortOrder === 'asc'
+					? (a[field] as number) - (b[field] as number)
+					: (b[field] as number) - (a[field] as number);
+			}
+		});
+
+		setSortField(field);
+		setSortOrder(newSortOrder);
+		setCoinData(sortedData);
+	};
+
 	return (
 		<section id='market' className='market-section'>
 			<div className='market-container'>
@@ -56,12 +81,54 @@ const Market: FC = () => {
 					<table className='market-table'>
 						<thead>
 							<tr>
-								<th>#</th>
-								<th>Coin</th>
-								<th>Price</th>
-								<th>24h Change</th>
-								<th>24h Volume</th>
-								<th>Market Cap</th>
+								{[
+									'#',
+									'Coin',
+									'Price',
+									'24h Change',
+									'24h Volume',
+									'Market Cap',
+								].map((header, idx) => {
+									const sortFieldMapping: { [key: string]: keyof ICoinData } = {
+										'#': 'rank',
+										Coin: 'name',
+										Price: 'price',
+										'24h Change': 'change24h',
+										'24h Volume': 'volume24h',
+										'Market Cap': 'marketCap',
+									};
+
+									const currentSortField = sortFieldMapping[header];
+
+									return (
+										<th key={idx}>
+											<div className='th-content'>
+												{header}
+												<span
+													className='sort-arrows'
+													onClick={() => handleSort(currentSortField)}
+												>
+													<div
+														className={
+															sortField === currentSortField &&
+															sortOrder === 'asc'
+																? 'arrow-up active-arrow'
+																: 'arrow-up'
+														}
+													></div>
+													<div
+														className={
+															sortField === currentSortField &&
+															sortOrder === 'desc'
+																? 'arrow-down active-arrow'
+																: 'arrow-down'
+														}
+													></div>
+												</span>
+											</div>
+										</th>
+									);
+								})}
 							</tr>
 						</thead>
 						<tbody>
