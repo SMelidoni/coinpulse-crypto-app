5ec6d6d20c06e1e2b463ea550a17ecd56b6d250e
SMelidoni <simone.melidoni@gmail.com>
2023-08-12 11:08:54 +0100

    Added initial 'Learn' section

---
+++ src/pages/learn/learn.component.tsx
@@ -0,0 +1,14 @@
+import './learn.styles.scss';
+import React, { FC } from 'react';
+
+const Learn: FC = () => {
+	return (
+		<section id='learn' className='learn-section'>
+			<div className='learn-container'>
+				<h1 className='title'>Learn</h1>
+			</div>
+		</section>
+	);
+};
+
+export default Learn;
